880960a46cc49bd9f13e9538aa979ea7264860f2
Kucing <dilkq@gmail.com>
2020-09-27 16:11:51 +0700

    building navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,15 +3,10 @@ import React from 'react'
 function Navbar() {
   return (
     // Navigation  
-    <nav className="flex fixed bg-gray-300 w-full top-0 right-0 p-4">
-      <div className="flex ml-3">
-        <button className="w-6 h-auto cursor-pointer">
-          <img src="/menu.svg" alt="menu" />
-        </button>
-      </div>
-      <div className="flex items-center">
+    <nav className="flex fixed items-center justify-center bg-white shadow w-full top-0 right-0 p-4">
+      <div className="flex self-center">
         <a href="#" className="p-3">
-          <img src="/kabarcovid-logo.svg" className="w-auto h-3" />
+          <img src="/kabarcovid-logo.svg" className="w-auto h-5" />
         </a>
       </div>
     </nav>
