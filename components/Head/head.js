de7b7c99cc14835ad1417920200fe0a37a9c5c71
Kucing <dilkq@gmail.com>
2021-01-07 14:57:12 +0700

    not change

--- components/Head/head.js
+++ components/Head/head.js
@@ -5,14 +5,14 @@ const HeadComponent = () => {
   return (
     <>
       <Head>
-        <title>Berita Terpercaya Dan Terupdate Covid-19</title>
+        <title>Berita Covid-19 Terpercaya Dan Terupdate </title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name="description" content=""></meta>
         <meta name="robots" content="index" />
         <link rel="canonical" href="" />
-        
+
         <meta property="og:title" content="" />
         <meta property="og:description" content="" />
         <meta property="og:image" content="" />
